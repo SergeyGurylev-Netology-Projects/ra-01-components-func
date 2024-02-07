0831b0f70c4f328d56f8f51a071dca5f76469daf
SergeyGurylev <ohg@mail.ru>
2024-02-07 13:05:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function ShopItemFunc(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-content">
+      <h2>{props.brand}</h2>
+      <h1>{props.title}</h1>
+      <h3>{props.description}</h3>
+      <div className="description">
+        {props.descriptionFull}
+      </div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{props.currency}{props.price.toFixed(2)}</div>
+        <button>Добавить в корзину</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default ShopItemFunc ;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ShopItemFunc from './App';
 import reportWebVitals from './reportWebVitals';
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ShopItemFunc {...item}/>
   </React.StrictMode>
 );
 
